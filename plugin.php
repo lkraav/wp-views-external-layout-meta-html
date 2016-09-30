1267fcbe01d04461a79a64f2c15d4eed49bfba9b
leho <leho@kraav.com>
2016-09-30 15:47:20 +0300

    hello WPV_External_Layout_Meta_HTML::layout_meta_html_by_theme()
    MOTIVATION
    
    * views get improvements over time, they should benefit all projects,
      even retroactively, with risk-free deployments
    
    * standardized templates/configurations, like `display-child-pages`
      could now be pulled into themes declaratively w/
      `{composer,package}.json`; each template needs to become individual
      repo
    
    * dev workflow gains visibility into infrastructure area previously in
      dark
    
    * view editing doesn't have revisions, very difficult to assign blame or
      recover from mistakes
    
    
    TODO
    
    * generalize backend location infrastructure?
    
    * filter admin layout meta html box, insert clarity copy? itmw, admin can
      manually insert a placeholder note
    
    * CSS + JS editor?

---
+++ plugin.php
@@ -0,0 +1,57 @@
+<?php
+/*
+Plugin Name: Toolset Views External Layout Meta HTML
+Plugin URI: https://conversionready.com
+Description: Filter view layout HTML definition with an external source
+Version: 2016.09.30
+Author: Leho Kraav
+Author URI: https://conversionready.com
+*/
+
+if ( is_admin() ) {
+    return;
+}
+
+final class WPV_External_Layout_Meta_HTML {
+
+    private static $layout_meta_html_by_theme_directory = 'wp-views';
+
+    public static function plugins_loaded() {
+
+        if ( ! defined( 'WPV_VERSION' ) ) {
+            return;
+        }
+
+        # plugins/wp-views/embedded/inc/wpv.class.php WP_Views::get_view_layout_settings()
+        add_filter( 'wpv_filter_override_view_layout_settings', [ __CLASS__, 'layout_meta_html_by_theme' ], 10, 2 );
+
+    }
+
+    # implements known theme location backend
+    public static function layout_meta_html_by_theme( $view_layout_settings, $view_id ) {
+
+        $view_post_name = get_post_field( 'post_name', $view_id );
+
+        $layout_meta_html_file = sprintf( '%s/%s/%s.html',
+            get_stylesheet_directory(),
+            static::$layout_meta_html_by_theme_directory,
+            $view_post_name
+        );
+
+        $layout_meta_html_file = apply_filters( 'wpv_external_layout_meta_html_file',
+            $layout_meta_html_file,
+            $view_layout_settings,
+            $view_id
+        );
+
+        if ( $layout_meta_html = @file_get_contents( $layout_meta_html_file ) ) {
+            $view_layout_settings['layout_meta_html'] = $layout_meta_html;
+        }
+
+        return $view_layout_settings;
+
+    }
+
+}
+
+add_action( 'plugins_loaded', [ 'WPV_External_Layout_Meta_HTML', 'plugins_loaded' ] );
