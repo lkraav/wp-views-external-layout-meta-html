225f6ed9dbc787e19f682c7f3fce937ea2d08f6e
leho <leho@conversionready.com>
2018-08-23 11:57:01 +0300

    phpcs-cr 1.0.0

--- plugin.php
+++ plugin.php
@@ -1,33 +1,51 @@
 <?php
-/*
-Plugin Name: Toolset Views External Layout Meta HTML
-Plugin URI: https://conversionready.com
-Description: Filter view layout HTML definition with an external source
-Version: 2016.10.04
-Author: Leho Kraav
-Author URI: https://conversionready.com
-*/
+/**
+ * Plugin Name: Toolset Views External Layout Meta HTML
+ * Plugin URI: https://conversionready.com
+ * Description: Filter view layout HTML definition with an external source
+ * Version: 2016.10.04
+ * Author: Leho Kraav
+ * Author URI: https://conversionready.com
+ *
+ * @package WPV_External_Layout_Meta_HTML
+ */
 
 if ( is_admin() ) {
     return;
 }
 
+/**
+ * Main
+ */
 final class WPV_External_Layout_Meta_HTML {
 
+    /**
+     * Look for templates in this sub-directory.
+     *
+     * @var string
+     */
     private static $layout_meta_html_by_theme_directory = 'wp-views';
 
+    /**
+     * Init
+     */
     public static function plugins_loaded() {
 
         if ( ! defined( 'WPV_VERSION' ) ) {
             return;
         }
 
-        # plugins/wp-views/embedded/inc/wpv.class.php WP_Views::get_view_layout_settings()
+        // @see plugins/wp-views/embedded/inc/wpv.class.php WP_Views::get_view_layout_settings()
         add_filter( 'wpv_filter_override_view_layout_settings', [ __CLASS__, 'layout_meta_html_by_theme' ], 10, 2 );
 
     }
 
-    # implements known theme location backend
+    /**
+     * Implements known theme location backend.
+     *
+     * @param array $view_layout_settings Configuration.
+     * @param int   $view_id View ID.
+     */
     public static function layout_meta_html_by_theme( $view_layout_settings, $view_id ) {
 
         $view_post_name = get_post_field( 'post_name', $view_id );
@@ -44,7 +62,10 @@ final class WPV_External_Layout_Meta_HTML {
             $view_id
         );
 
-        if ( $layout_meta_html = @file_get_contents( $layout_meta_html_file ) ) {
+        // Non-existent templates are safe to ignore.
+        $layout_meta_html = @file_get_contents( $layout_meta_html_file );
+
+        if ( $layout_meta_html ) {
 
             $layout_meta_html_debug = sprintf( "<!-- %s::%s -> %s -->\n",
                 __CLASS__,
